7a7348e883cb5609ac5e530b199f69856677afed
toha245 <mdabdulawal2001@gmail.com>
2025-09-09 14:54:39 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
-export default {
->   content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
